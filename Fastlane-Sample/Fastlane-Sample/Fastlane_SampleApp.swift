16341d9fb8bf475eb7e799f70a9150524bd3f5de
Nathália Cardoso <crds.nathalia@gmail.com>
2023-06-06 20:26:11 -0300

    setup inicial

---
+++ Fastlane-Sample/Fastlane-Sample/Fastlane_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fastlane_SampleApp.swift
+//  Fastlane-Sample
+//
+//  Created by Nathalia Cardoso on 06/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Fastlane_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
