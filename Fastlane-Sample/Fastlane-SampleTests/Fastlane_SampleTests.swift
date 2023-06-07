51ec95442c1391da0589056eb0e65a000c8ce472
Nathália Cardoso <crds.nathalia@gmail.com>
2023-06-06 21:46:31 -0300

    unit_test

---
+++ Fastlane-Sample/Fastlane-SampleTests/Fastlane_SampleTests.swift
@@ -0,0 +1,16 @@
+//
+//  Fastlane_SampleTests.swift
+//  Fastlane-SampleTests
+//
+//  Created by Nathalia Cardoso on 06/06/23.
+//
+
+import XCTest
+
+final class Fastlane_SampleTests: XCTestCase {
+
+    func testExample() throws {
+        XCTAssertEqual(2+2, 4)
+    }
+
+}
